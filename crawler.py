b3a6c6ee397272504cb421f6488e03a80880ec62
Suzanne <suzanneloures@gmail.com>
2017-08-18 22:56:29 -0300

    insercao busca de titulo

--- crawler.py
+++ crawler.py
@@ -4,3 +4,5 @@ from robobrowser import RoboBrowser
 # Browse to Rap Genius
 browser = RoboBrowser(history=True)
 browser.open('http://wiki.dcc.ufba.br/BSI')
+browser.select('a')
+browser.find_all(['h1','h2','h3','h4','h5','h6'])

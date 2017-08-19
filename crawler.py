01ec37b1158a2252df0ec55ba78d69c696f1ddb5
Ramon Oliveira Barbosa <ramonflmr@gmail.com>
2017-08-18 23:29:58 -0300

    pegando titulos

--- crawler.py
+++ crawler.py
@@ -1,8 +1,17 @@
 import re
 from robobrowser import RoboBrowser
 
+vetor_links = []
+vetor_titulos = []
+
 # Browse to Rap Genius
 browser = RoboBrowser(history=True)
 browser.open('http://wiki.dcc.ufba.br/BSI')
-browser.select('a')
+links = browser.select('a')
 browser.find_all(['h1','h2','h3','h4','h5','h6'])
+
+for link in links:
+    if(not link['href'].startswith('http') and link['href'] not in vetor_links):
+        vetor_links.append(link['href'])
+        browser.open('http://wiki.dcc.ufba.br' + link['href'])
+        print(browser.select('title')[0])

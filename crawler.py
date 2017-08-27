74c727e517ff3124c3b16e0e36a65de547a59579
ramonbrbs <ramonflmr@gmail.com>
2017-08-27 00:22:37 -0300

    Rodando a busca quase completa

--- crawler.py
+++ crawler.py
@@ -1,44 +1,43 @@
-import re
 from robobrowser import RoboBrowser
-from .pagina2 import Pagina
+from pagina2 import Pagina
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///banco.db', echo=True)
-vetor_links = []
-vetor_titulos = []
-vetor_paginas = []
+def url_navegavel(url):
+    global url_pagina_inicial
+    if(url.startswith("/")):
+        return url_pagina_inicial + url
 
-vetor_links.insert(1, [])
+    return url
 
-# Browse to Rap Genius
-url_pagina_inicial = 'http://www.dmat.ufba.br'
-nivel = 1
-
-
-
-browser = RoboBrowser() # history=true volta para pagina anterior
-browser.open(url_pagina_inicial)
-links = browser.select('a')
-browser.find_all(['h1','h2','h3','h4','h5','h6'])
-
-for link in links:
-    if(not link['href'].startswith('http') and link['href'] not in vetor_links):
-        vetor_links.append(link['href'])
-        browser.open(url_pagina_inicial + link['href'])
-
-# funcao captura todos os titulos e links da pagina e salva no vetor
-
-def captura (url):
+def valida_url(url):
     global vetor_links
-    global vetor_titulos
-    global engine
+    if(url.startswith('http') ):
+        return False
+    
+    if(url in vetor_links ):
+        return False
+    
+    if(url.startswith("#")):
+        return False
+    if(url.startswith('javascript')):
+        return False
+    
+    if(url == '/'):
+        return False
+    
+    return True
+
+def captura (pagina):
+    global vetor_paginas
     global nivel
+    global vetor_links
 
+    vetor_links.append(pagina.url)
     browser = RoboBrowser()
-    browser.open(url)
+    browser.open(url_navegavel(pagina.url))
 
-    pagina = Pagina()
-    pagina.url = url
+    
+    
     pagina.titulo = browser.select('title')
     pagina.h1 = browser.select('h1')
     pagina.h2 = browser.select('h2')
@@ -49,23 +48,58 @@ def captura (url):
     pagina.negrito = browser.find_all(['strong', 'b'])
     links = browser.select('a')
 
-    for posicao in links:
-        if(not link['href'].startswith('http') and link['href'] not in vetor_links):
+    if(len(vetor_paginas) <= nivel+1):
+        vetor_paginas.insert(nivel+1, [])
+
+    for link in links:
+        if(link.has_attr('href') and valida_url(link['href'])):
+            p = Pagina(link['href'])
+            vetor_paginas[nivel + 1].append(p)
             vetor_links.append(link['href'])
 
-    pagina = Pagina()
-    pagina.link = url
-    pagina.titulo = titulo
-    pagina.h1 = h1
-    pagina.h2 = h2
-    pagina.h3 = h3
-    pagina.h4 = h4
-    pagina.h5 = h5
-    pagina.h6 = h6
-    pagina.strong = strong
-    pagina.negrito = negrito
+#engine = create_engine('sqlite:///banco.db', echo=True)
+#vetor_links = []
+vetor_titulos = []
+vetor_paginas = []
+vetor_links = []
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    session.add(pagina)
-    session.commit()
+vetor_paginas.insert(0, [])
+
+# Browse to Rap Genius
+url_pagina_inicial = 'http://ufba.br'
+nivel = 0
+posicao_largura = 0
+
+p_inicial = Pagina(url_pagina_inicial)
+
+vetor_paginas[0].append(p_inicial)
+#browser = RoboBrowser() # history=true volta para pagina anterior
+#browser.open(url_pagina_inicial)
+#links = browser.select('a')
+#browser.find_all(['h1','h2','h3','h4','h5','h6'])
+
+#for link in links:
+#    if(not link['href'].startswith('http') and link['href'] not in vetor_links):
+#        vetor_links.append(link['href'])
+#        browser.open(url_pagina_inicial + link['href'])
+
+# funcao captura todos os titulos e links da pagina e salva no vetor
+
+while(vetor_paginas[nivel] is not None):
+    if(len(vetor_paginas[nivel]) == posicao_largura ):
+        nivel += 1
+        posicao_largura = 0
+    else:
+        captura(vetor_paginas[nivel][posicao_largura])
+        posicao_largura += 1
+    
+
+
+
+
+
+
+    #Session = sessionmaker(bind=engine)
+    #session = Session()
+    #session.add(pagina)
+    #session.commit()
--- pagina2.py
+++ pagina2.py
@@ -1,13 +1,15 @@
 class Pagina(object):
-    def __init__ (self, url, titulo, h1, h2, h3, h4, h5, h6, negrito):
+    filhos = []
+
+    def __init__ (self, url, titulo = '', h1='', h2='', h3='', h4='', h5='', h6='', negrito=''):
         self.titulo = titulo
         self.url = url
         self.titulo = titulo
         self.h1 = h1
         self.h2 = h2
-        self.h3  h3
+        self.h3 = h3
         self.h4 = h4
-        self.h5 h5
+        self.h5 = h5
         self.h6 = h6
         self.negrito = negrito
 

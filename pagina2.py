47a6e71e2a720b5eea89d6adcb95a314cfebabec
Suzanne <suzanneloures@gmail.com>
2017-08-26 21:11:26 -0300

    fazendo classe para salvar info

---
+++ pagina2.py
@@ -0,0 +1,8 @@
+class Pagina(object):
+    titulo = 'abs'
+
+    def __init__ (self, url, titulo, h1, h2, h3, h4, h5, h6, negrito):
+        self.titulo = titulo
+        self.url = url
+        self.titulo = titulo
+    

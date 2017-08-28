4dbc471e2ef0619d5dcaf2c823b7e52f0b74cf8a
ramonbrbs <ramonflmr@gmail.com>
2017-08-28 01:19:43 -0300

    busca na web

--- server.py
+++ server.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return current_app.send_static_file('chat.html')
+    return current_app.send_static_file('webchat.html')
 
 @app.route('/perguntar', methods = ['POST'])
 def perguntar():
     pergunta = request.form['pergunta']
     retorno = buscar(pergunta)
     if len(retorno) == 0:
-        return "<br/> Não encontrei nada relacionado a sua questão. Por favor reformule"
-    return '<br/>'+'<br/>'.join(retorno)
+        return "<br/> Não encontrei nada relacionado a sua questão. Tente palavras chaves"
+    return '<br/> Verifiquei que as seguintes páginas correspondem à sua pergunta: '+'<br/>'.join(retorno)
